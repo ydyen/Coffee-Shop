5e379071669166d967ed9e0c2c38c2600cca78d9
Yoeuth Danny Yen <danny.y.yen@gmail.com>
2020-06-15 02:21:05 +0300

    add setting.js as secret

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 const app = require('./app');
 const server = http.createServer(app);
 const mongoose = require('mongoose');
-const uri = require('./setting');
+const uri = require('./db/setting');
 
 //Connection to Database
 async function main() {
