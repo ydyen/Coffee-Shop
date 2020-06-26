64e00635db00c57c80abbe9ce5f07cca4f1721bc
Yoeuth Danny Yen <danny.y.yen@gmail.com>
2020-06-26 13:26:38 +0300

    rm config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-
-//password will be updated after the video recording
-const uri = "mongodb+srv://admin:" + "Xn4CCeP0mJ5Y3b91" + "@cluster0-xwuwa.mongodb.net/coffeeshop?retryWrites=true&w=majority";
-
-module.exports = uri;
